57f1707978fad71383e110361b0a7f09914733f3
bmalkes <bmalkes@gmail.com>
2021-11-09 19:30:33 +0200

    Feat:add CustomInput Component

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import TaskItem from "./components/TaskItem";
+import Tasks from "./components/Tasks";
+import "./App.css";
+
+const App = () => {
+    return <Tasks />;
+};
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./Tasks.scss";
+
+const Tasks = () => {
+    const [task, setTask] = useState([]);
+
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:8000/tasks");
+            setTask(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    return (
+        <div className="tasks-container">
+            <h2>My Tasks</h2>
+
+            <div className="last-tasks">
+                <h3>Last Tasks</h3>
+                <div className="tasks-list">
+                    {task
+                        .filter((task) => task.isCompleted === false)
+                        .map((lastTask) => (
+                            <p>{lastTask.description}</p>
+                        ))}
+                </div>
+            </div>
+
+            <div className="completed-tasks">
+                <h3>Completed Tasks</h3>
+                <div className="tasks-list">
+                    {task
+                        .filter((task) => task.isCompleted)
+                        .map((completedTask) => (
+                            <p>{completedTask}</p>
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;
