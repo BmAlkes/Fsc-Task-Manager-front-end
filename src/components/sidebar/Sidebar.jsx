8c0b054dad7198fefcb88bcad99795a9e2f7edb4
bmalkes <bmalkes@gmail.com>
2021-11-22 13:56:32 +0200

    feat:add navigation

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,16 +1,22 @@
 import CustomButton from "../../CustomButton/CustomButton";
+import { useNavigate } from "react-router-dom";
 
 import logo from "../../assets/image/logo.png";
 import "./Sidebar.scss";
 
 const SideBar = () => {
+    const navigation = useNavigate();
+
+    const handleLogout = () => {
+        navigation("/login");
+    };
     return (
         <div className="sidebar-container">
             <div className="logo">
                 <img src={logo} alt="bmalkes" />
             </div>
             <div className="sign-out">
-                <CustomButton>Logout</CustomButton>
+                <CustomButton onClick={handleLogout}>Logout</CustomButton>
             </div>
         </div>
     );
--- src/pages/pages/Login.jsx
+++ src/pages/pages/Login.jsx
@@ -1,13 +1,19 @@
 import "./Login.scss";
 import logo from "../../assets/image/logo.png";
 import CustomButton from "../../CustomButton/CustomButton";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const navigation = useNavigate();
+    const handleSignIn = () => {
+        navigation("/");
+    };
+
     return (
         <div className="login-container">
             <img src={logo} alt="bmalkes" />
             <div className="button-container">
-                <CustomButton>Login</CustomButton>
+                <CustomButton onClick={handleSignIn}>Login</CustomButton>
             </div>
         </div>
     );
